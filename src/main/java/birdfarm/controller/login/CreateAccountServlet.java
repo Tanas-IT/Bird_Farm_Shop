f05d20c8ec3d92efd25074fdd5af12171ad6674d
Sy Truong <syttse173472@fpt.edu.vn>
2023-09-24 17:07:19 +0700

    sua Login trisy

---
+++ src/main/java/birdfarm/controller/login/CreateAccountServlet.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birdfarm.controller.login;
+
+import birdfarm.dao.UserDAO;
+import birdfarm.dto.UserDTO;
+import birdfarm.util.RegistrationCreateError;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.UUID;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "CreateAccountServlet", urlPatterns = {"/CreateAccountServlet"})
+public class CreateAccountServlet extends HttpServlet {
+
+    private final String RESULT_PAGE = "Register.html";
+    private final String LOGIN_PAGE = "Login.jsp";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url=RESULT_PAGE;
+        String uniqueID = UUID.randomUUID().toString();
+        String idUser = uniqueID.substring(0, 5);
+        String username = request.getParameter("txtUsername");
+        String password = request.getParameter("txtPassword");
+        String confirm = request.getParameter("txtConfirm");
+        String fullName = request.getParameter("txtFullName");
+        boolean foundErr = false;
+        RegistrationCreateError Error = new RegistrationCreateError();
+        try {
+            //1.Check all user's errors
+            if (username.trim().length() < 6
+                    || username.trim().length() > 20) {
+                foundErr = true;
+                Error.setUsernameLengthErr("Username is inputed from 6 to 20 characters");
+            }
+            if (password.trim().length() < 8
+                    || password.trim().length() > 30) {
+                foundErr = true;
+                Error.setPasswordLengthErr("Password is inputed from 8 to 30 characters");
+            } else if (!confirm.trim().equals(password.trim())) {
+                foundErr = true;
+                Error.setConfirmNotMatched("Confirm must match password");
+            }
+            if (fullName.trim().length() < 2
+                    || fullName.trim().length() > 20) {
+                foundErr = true;
+                Error.setFullNameLengthErr("Phone number inputed from 10 to 11 characters");
+            }
+            if (foundErr) {
+                request.setAttribute("CREATE_ERROR", Error);
+            } else {
+                //2.Call DAO
+                UserDAO dao = new UserDAO();
+                UserDTO account = new UserDTO(idUser,username, password, fullName,4);
+                boolean result = dao.createAccount(account);
+                //3.Process Result 
+                if (result) {
+                    url = LOGIN_PAGE;
+                }//Creating action is successful
+            }//do nothhing 
+        } catch (NamingException ex) {
+            log("CreateNewAccount_naming" + ex.getMessage());
+        } catch (SQLException ex) {
+            log("CreateNewAccount_SQL" + ex.getMessage());
+            Error.setUsernameIsExisted(username + " is Existed in system");
+            request.setAttribute("CREATE_ERROR", Error);
+        } finally {
+            RequestDispatcher rd = request.getRequestDispatcher(url);
+            rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/main/webapp/Login.jsp
+++ src/main/webapp/Login.jsp
@@ -8,50 +8,50 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Login</title> 
-    
-    <link rel="stylesheet" href="css/Google.css">
-    <link rel="stylesheet" href="css/Login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-  </head>
-  <body>
-    <div class="container">
-      <div class="wrapper">
-        <div class="title"><span>Login Form</span></div>
-        <form action="DispatchServlet" method="post">
-          <div class="row">
-            <i class="fas fa-user"></i>
-            <input name="txtUsername" type="text" placeholder="Username" required>
-          </div>
-          <div class="row">
-            <i class="fas fa-lock"></i>
-            <input name="txtPassword" type="password" placeholder="Password" required>
-          </div>
-          <div class="pass"><a href="#">Forgot password?</a></div>
-          <div class="row button">
-              <input type="submit" value="Login" name="btAction">
-          </div>
-          <div class="signup-link">Not a member? <a href="createNewAccount.html">Signup now</a></div>
-          <div class="conainer">
-      <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8084/SWPLogin/LoginGoogleHandler&response_type=code
-    &client_id=91807241555-9kgfge8t87k8lheot7cg5gs4u9avused.apps.googleusercontent.com&approval_prompt=force">
-        <div class="g-sign-in-button">
-          <div class="content-wrapper">
-            <div class="logo-wrapper">
-              <img src="https://developers.google.com/identity/images/g-logo.png">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title> 
+
+        <link rel="stylesheet" href="css/Google.css">
+        <link rel="stylesheet" href="css/Login.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="wrapper">
+                <div class="title"><span>Login</span></div>
+                <form action="DispatchServlet" method="post">
+                    <div class="row">
+                        <i class="fas fa-user"></i>
+                        <input name="txtUsername" type="text" placeholder="Username" required>
+                    </div>
+                    <div class="row">
+                        <i class="fas fa-lock"></i>
+                        <input name="txtPassword" type="password" placeholder="Password" required>
+                    </div>
+                    <div class="pass"><a href="#">Forgot password?</a></div>
+                    <div class="row button">
+                        <input type="submit" value="Login" name="btAction">
+                    </div>
+                    <div class="signup-link">Not a member? <a href="Register.html">Register</a></div>
+                    <div class="conainer">
+                        <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8084/SWPLogin/LoginGoogleHandler&response_type=code
+                           &client_id=91807241555-9kgfge8t87k8lheot7cg5gs4u9avused.apps.googleusercontent.com&approval_prompt=force">
+                            <div class="g-sign-in-button">
+                                <div class="content-wrapper">
+                                    <div class="logo-wrapper">
+                                        <img src="https://developers.google.com/identity/images/g-logo.png">
+                                    </div>
+                                    <span class="text-container">
+                                        <span>Sign in with Google</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                </form>
             </div>
-            <span class="text-container">
-              <span>Sign in with Google</span>
-            </span>
-          </div>
         </div>
-      </a>
-    </div>
-          
-        </form>
-      </div>
-    </div>
-  </body>
+    </body>
 </html>
